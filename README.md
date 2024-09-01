8dbb7784526b0f7d89e10df9ff117c64972456c2
athrvk <atharvakusumbia@gmail.com>
2024-09-01 19:51:18 +0530

    add readme, install script

---
+++ README.md
@@ -0,0 +1,18 @@
+# Sitemap Crawler
+
+A web crawler service which fetches the site map of the current domain.
+Its built using express, cheerio and react.
+
+This project is created on linux, so it might not work on Windows(haven't tested it).
+
+## Installation
+
+Clone the repository and install the dependencies using `npm install`.
+You don't have change directory, its done automatically.
+
+## Usage
+
+Start the server using `npm start`.
+
+Open http://localhost:3000 in your browser.
+
---
+++ build.js
@@ -0,0 +1,43 @@
+const exec = require('child_process').exec;
+
+exec('cd client && npm install', (error, stdout, stderr) => {
+    console.log('Installing dependencies for server and client...');
+    console.log('-------------------------');
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`client: ${stdout}`);
+})
+
+exec('cd server && npm install', (error, stdout, stderr) => {
+    console.log('-------------------------');
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`server: ${stdout}`);
+})
+
+exec('cd client && npm run build', (error, stdout, stderr) => {
+    console.log('Building client...');
+    console.log('-------------------------');
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`client: ${stdout}`);
+})
+
